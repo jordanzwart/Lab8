7c69e47a0e7b3f80f1c55982f851e222c1b2c60c
jordanzwart <ZWART.JORDAN20@GMAIL.COM>
2018-01-31 23:36:03 -0500

    comments added

--- src/BattingStats.java
+++ src/BattingStats.java
@@ -14,79 +14,80 @@ public class BattingStats {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String letter = "y";
+		// creates an empty array
 		int[] storeAtBat = null;
-		
+
 		System.out.println("Welcome to Batting Average Calculator!");
-		while(letter.equalsIgnoreCase("y")){
-		System.out.println("Enter number of at bat: ");
-		
-		int atBats = scan.nextInt();
-		
-		System.out.println("0 = out, 1=single, 2=double, 3=triple, 4=home run");
-		storeAtBat = new int[atBats];
-		
-		for (int i=0; i < atBats; i++){
-			System.out.println("Results for at-bat " + (i + 1) + ":");
-			storeAtBat[i] = scan.nextInt();
-			scan.nextLine();
-			
-			
-				while((storeAtBat[i] < 0 || storeAtBat[i] > 4)){
-				System.out.println("Not a valid result for at-bat. Result must be between 0-4");
-				System.out.println("Results for at-bat " + (i +1) + ":");
+		while (letter.equalsIgnoreCase("y")) {
+			System.out.println("Enter number of at bat: ");
+			// user enters how many at bats they want
+			int atBats = scan.nextInt();
+
+			System.out
+					.println("0 = out, 1=single, 2=double, 3=triple, 4=home run");
+
+			// the empty array now becomes how many at bats the user has inputed
+			storeAtBat = new int[atBats];
+			// for loop now cycles through storing the results entered into the
+			// correct spot
+			for (int i = 0; i < atBats; i++) {
+				System.out.println("Results for at-bat " + (i + 1) + ":");
 				storeAtBat[i] = scan.nextInt();
+				scan.nextLine();
+
+				// if the user is out of bounds it will notify that the
+				// selection of lower than 0 or bigger
+				// than 4 is an incorrect input for base hits
+				while ((storeAtBat[i] < 0 || storeAtBat[i] > 4)) {
+					System.out
+							.println("Not a valid result for at-bat. Result must be between 0-4");
+					System.out.println("Results for at-bat " + (i + 1) + ":");
+					storeAtBat[i] = scan.nextInt();
 				}
-			
-		}
-		int sum = 0;
-		for(int i = 0; i < storeAtBat.length; i++){
-			sum+= storeAtBat[i];
-		}
-			//sluggingpercentPercentage
-		int baseHit = 0;
-		for(int i = 0; i < storeAtBat.length; i++){
-			if (storeAtBat[i] > 0){
-			baseHit++;
+
 			}
+			// this calculates the total amount of bases the user got
+			int sum = 0;
+			for (int i = 0; i < storeAtBat.length; i++) {
+				sum += storeAtBat[i];
+			}
+			// this calculates the total numbers of basehits
+			int baseHit = 0;
+			for (int i = 0; i < storeAtBat.length; i++) {
+				if (storeAtBat[i] > 0) {
+					baseHit++;
+				}
+			}
+			System.out.println("Hits: " + baseHit);
+			System.out.println("Total Bases: " + sum);
+			// two seperate methods calling the avg and the slugging equations
+			System.out.println(avg(baseHit, atBats));
+			System.out.println(slg(sum, atBats));
+
+			System.out.println("Another Batter? Enter: (y/n)");
+			// case statments for yes and no to continue or not
+			letter = scan.nextLine();
+			switch (letter) {
+			case "y":
+				break;
+			case "Y":
+				break;
+			case "n":
+				System.out.println("goodbye");
+				break;
+			case "N":
+				System.out.println("goodbye");
+				break;
+			default:
+				System.out.println("Not a valid input, try again");
+				letter = scan.nextLine();
+				break;
+			}
+
 		}
-		System.out.println("Hits: " + baseHit);
-		System.out.println("Total Bases: " + sum);
-		
-		System.out.println(avg(baseHit,atBats));
-		System.out.println(slg(sum,atBats));
-		
-		System.out.println("Another Batter? Enter: (y/n)");
-		
-		letter = scan.nextLine();
-//		switch(letter){
-//		case "y":
-//			break;
-//		case "Y":
-//			break;
-//		case "n":
-//			System.out.println("goodbye");
-//			break;
-//		case "N":
-//			System.out.println("goodbye");
-//			break;
-//		default:
-//			System.out.println("Not a valid input");
-//			break;
-//		}
-		
+
 	}
-		
-	}
-
-		
-		
-		
-		
-
-
-
-
-
+//slugging percentage and batting average calculations
 	public static BigDecimal avg(int hits, int atBats) {
 		double average = (double) hits / (double) atBats;
 		BigDecimal b = new BigDecimal(average);
@@ -100,5 +101,3 @@ public class BattingStats {
 		return b.setScale(3, BigDecimal.ROUND_HALF_UP);
 	}
 }
-
-
